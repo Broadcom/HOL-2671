0ac3f79b10f3c7a5b7f9110f0c6d3401b65ad94c
Nick Robbins <nrobbins@vmware.com>
2025-07-17 18:21:23 -0500

    updates

--- HOL-2x71/2x71_podsetup/adjustomatic.py
+++ HOL-2x71/2x71_podsetup/adjustomatic.py
@@ -20,25 +20,27 @@ def main():
         lsf.write_output("Configuring NSX T App profiles")   
         #add fast tcp and udp nsxt lb app profiles
         #prepare the http connection to NSX Manager
-        session = requests.Session()
-        session.verify = False
-        session.auth = ('admin', os.environ["AVICTRL_PASS"])
-        nsx_mgr = 'https://nsx-wld01-a.site-a.vcf.lab'
-        fast_tcp_data = {
-            'display_name': 'custom-fast-tcp',
-            'idle_timeout': '1700',
-            'close_timeout': '8',
-            'resource_type': 'LBFastTcpProfile'
-            }
-        fast_udp_data = {
-            'display_name' : 'custom-fast-udp',
-            'idle_timeout':  '330',
-            'resource_type': 'LBFastUdpProfile'
-            }
-        tcp_result = session.put(f"{nsx_mgr}/policy/api/v1/infra/lb-app-profiles/custom-fast-tcp", json=fast_tcp_data)
-        lsf.write_output(f"Result code - {tcp_result.status_code}, Error text - {tcp_result.text}")
-        udp_result = session.put(f"{nsx_mgr}/policy/api/v1/infra/lb-app-profiles/custom-fast-udp", json=fast_udp_data)
-        lsf.write_output(f"Result code - {udp_result.status_code}, Error text - {udp_result.text}")
+        print(os.environ)
+        lsf.write_output(os.environ)
+        # session = requests.Session()
+        # session.verify = False
+        # session.auth = ('admin', os.environ['AVICTRL_PASS'])
+        # nsx_mgr = 'https://nsx-wld01-a.site-a.vcf.lab'
+        # fast_tcp_data = {
+        #     'display_name': 'custom-fast-tcp',
+        #     'idle_timeout': '1700',
+        #     'close_timeout': '8',
+        #     'resource_type': 'LBFastTcpProfile'
+        #     }
+        # fast_udp_data = {
+        #     'display_name' : 'custom-fast-udp',
+        #     'idle_timeout':  '330',
+        #     'resource_type': 'LBFastUdpProfile'
+        #     }
+        # tcp_result = session.put(f"{nsx_mgr}/policy/api/v1/infra/lb-app-profiles/custom-fast-tcp", json=fast_tcp_data)
+        # lsf.write_output(f"Result code - {tcp_result.status_code}, Error text - {tcp_result.text}")
+        # udp_result = session.put(f"{nsx_mgr}/policy/api/v1/infra/lb-app-profiles/custom-fast-udp", json=fast_udp_data)
+        # lsf.write_output(f"Result code - {udp_result.status_code}, Error text - {udp_result.text}")
     except Exception as e:
         lsf.write_output(e)
         try:
@@ -48,6 +50,48 @@ def main():
             pass 
         lsf.write_output('Adjustomatic failed at nsxt app profile create')   
         #lsf.labfail('Adjustomatic failed at nsxt app profile create')
+
+    try:
+        lsf.write_output("Running first stages playbook")   
+        # Playbook to run final config steps
+        result = subprocess.run(["/home/holuser/.local/bin/ansible-playbook", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/labconfig_firststage.yaml", 
+            "-i", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/inventory.yml", "--vault-password-file", 
+            "/home/holuser/vaultsecret.txt"], capture_output=True, text=True, check=True)
+        lsf.write_output(result)
+        try:
+            lsf.write_output(result.stdout)
+        except:
+            pass
+    except Exception as e:
+        lsf.write_output(e)
+        try:
+            lsf.write_output(e.stdout)
+            lsf.write_output(e.stderr)
+        except:
+            pass   
+        lsf.write_output('Adjustomatic failed at avitweaker') 
+        lsf.labfail('Adjustomatic failed at avitweaker')
+
+    try:
+        lsf.write_output("Running final stages playbook")   
+        # Playbook to run final config steps
+        result = subprocess.run(["/home/holuser/.local/bin/ansible-playbook", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/avi_configs/avi_config.yml", 
+            "-i", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/avi_configs/inv_sitea.yml", "--vault-password-file", 
+            "/home/holuser/vaultsecret.txt"], capture_output=True, text=True, check=True)
+        lsf.write_output(result)
+        try:
+            lsf.write_output(result.stdout)
+        except:
+            pass
+    except Exception as e:
+        lsf.write_output(e)
+        try:
+            lsf.write_output(e.stdout)
+            lsf.write_output(e.stderr)
+        except:
+            pass   
+        lsf.write_output('Adjustomatic failed at avitweaker') 
+        lsf.labfail('Adjustomatic failed at avitweaker')
 
     # try:
     #     lsf.write_output("Running final stages playbook")   
