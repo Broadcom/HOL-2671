d5ef746e69675d4e0c449d23ff85abbe6d82dc89
Nick Robbins <nrobbins@vmware.com>
2025-07-18 10:08:00 -0500

    more fixes

--- HOL-2x71/2x71_podsetup/adjustomatic.py
+++ HOL-2x71/2x71_podsetup/adjustomatic.py
@@ -8,7 +8,7 @@ def main():
     import os
     import requests
     import json
-    import file
+  
     os.umask(0o0000)  ## lsfunctions sets a umask without execute, need to override or script-running things will die
     password_file = "/home/holuser/creds.txt"
     os.environ["http_proxy"] = "http://proxy:3128"
@@ -17,8 +17,8 @@ def main():
     os.environ["HTTP_PROXY"] = "http://proxy:3128"
     os.environ["HTTPS_PROXY"] = "http://proxy:3128"
     os.environ["NO_PROXY"] = "localhost,127.0.0.0/8,::1,site-a.vcf.lab,10.0.0.87,10.0.1.87,10.0.0.0/8"  
-    os.environ["AVICTRL_PASSWORD"] = file.readFile(password_file)
-    os.environ["TF_VAR_nsxt_password"] = file.readFile(password_file)
+    os.environ["AVICTRL_PASSWORD"] = open(password_file, 'r').read()
+    os.environ["TF_VAR_nsxt_password"] = open(password_file, 'r').read()
 
     try:
         lsf.write_output("Configuring NSX T App profiles")   
@@ -58,8 +58,8 @@ def main():
     try:
         lsf.write_output("Running first stages playbook")   
         # Playbook to run final config steps
-        result = subprocess.run(["/home/holuser/py312venv/bin/ansible-playbook", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/labconfig_firststage.yaml", 
-            "-i", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/inventory.yml", "--vault-password-file", 
+        result = subprocess.run(["/usr/bin/ansible-playbook", "/vpodrepo/2026-labs/2671/HOL-2x71/2x71_podsetup/labconfig_firststage.yaml", 
+            "-i", "/vpodrepo/2026-labs/2671/HOL-2x71/2x71_podsetup/inventory.yml", "--vault-password-file", 
             "/home/holuser/vaultsecret.txt"], capture_output=True, text=True, check=True)
         lsf.write_output(result)
         try:
@@ -79,8 +79,8 @@ def main():
     try:
         lsf.write_output("Running final stages playbook")   
         # Playbook to run final config steps
-        result = subprocess.run(["/home/holuser/py312venv/bin/ansible-playbook", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/avi_configs/avi_config.yml", 
-            "-i", "/vpodrepo/2025-labs/2571/HOL-2x71/2x71_podsetup/avi_configs/inv_sitea.yml", "--vault-password-file", 
+        result = subprocess.run(["/usr/bin/ansible-playbook", "/vpodrepo/2026-labs/2671/HOL-2x71/2x71_podsetup/avi_configs/avi_config.yml", 
+            "-i", "/vpodrepo/2026-labs/2671/HOL-2x71/2x71_podsetup/avi_configs/inv_sitea.yml", "--vault-password-file", 
             "/home/holuser/vaultsecret.txt"], capture_output=True, text=True, check=True)
         lsf.write_output(result)
         try:
